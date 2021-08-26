12175fd41c389690b9ce74eb8f6df6a399c38d8e
Mafe Callejon <mfcf@Mafes-MacBook-Pro.local>
2021-08-26 13:06:56 +0100

    Added color module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { createLegendContainer, createDiscreteColorLegend, createContinuousColorLegend, createContinuousSizeLegend } from "@flourish/legend";
 
+import createColors from "@flourish/colors";
+
 export var state = {
   legend_container: {},
   legend_categorical: {},
   legend_continuous: {},
-  legend_size: {}
+  legend_size: {},
+  color:{}
   // The current state of template. You can make some or all of the properties
   // of the state object available to the user as settings in settings.js.
 };
@@ -13,6 +16,7 @@ const legend_container = createLegendContainer(state.legend_container);
 const legend_categorical = createDiscreteColorLegend(state.legend_categorical);
 const legend_continuous = createContinuousColorLegend(state.legend_continuous);
 const legend_size = createContinuousSizeLegend(state.legend_size);
+const colors = createColors(state.color);
 
 function init() {
 
@@ -31,35 +35,44 @@ export var data = {};
 // Each of the 'datasets' in data.json file will be available as properties of the data.
 
 
-
 export function update() {
+  //Test whether the function is being called
+  console.log("Hi! I'm being called (also, long live the Queen)")
+  
+  // var legend_items = [
+  //   {
+  //     label: "Brazil",
+  //     color: "#ff0000"
+  //   },
+  //   {
+  //     label: "Argentina",
+  //     color: "#000000"
+  //   }
+  // ]
+  // legend_categorical.data(legend_items);
 
-  var legend_items = [
-    {
-      label: "Brazil",
-      color: "#ff0000"
-    },
-    {
-      label: "Argentina",
-      color: "#000000"
-    }
-  ]
-  legend_categorical.data(legend_items);
+  // var legend_continuous_items = [
+  //   {
+  //     label: 10,
+  //     color: "#ff0000"
+  //   },
+  //   {
+  //     label: 0,
+  //     color: "#000000"
+  //   }
+  // ]
 
-  var legend_continuous_items = [
-    {
-      label: 10,
-      color: "#ff0000"
-    },
-    {
-      label: 0,
-      color: "#000000"
-    }
-  ]
+  // legend_continuous.data(legend_continuous_items);
 
-  legend_continuous.data(legend_continuous_items);
+  // legend_container.update()
 
-  legend_container.update()
+  const domain = [1, 2, 3]
+
+  colors.updateColorScale(domain)
+
+  const color1 = colors.getColor(1)
+
+  console.log(color1)
   // The update function is called whenever the user changes a data table or settings
   // in the visualisation editor, or when changing slides in the story editor.
 
